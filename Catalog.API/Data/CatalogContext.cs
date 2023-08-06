d6d164b9a5c13784d73b64d70149c9d11d297710
Weslley Felix <weslleypfelix@gmail.com>
2023-08-06 20:11:15 -0300

    feat: creating entity and context classes

---
+++ Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>
+                ("DatabaseSettings:ConnectionString"));
+
+            var dataBase = client.GetDatabase(configuration.GetValue<string>
+                ("DatabaseSettings:DatabaseName"));
+
+            Products = dataBase.GetCollection<ProductEntity>(configuration.GetValue<string>
+                ("DatabaseSettings:CollectionName"));
+
+            CatalogContextSeed.SeedData(ProductEntity);
+        }
+        public IMongoCollection<ProductEntity> Products { get; }
+    }
+}
